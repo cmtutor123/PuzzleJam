4bb7d99641afea189268ff5555112d300aa257da
Caleb Tutor <cmtutor123@gmail.com>
2023-08-26 20:21:58 -0400

    started working on combat manager

--- Puzzle Jam/Assets/Scripts/Managers/CombatManager.cs
+++ Puzzle Jam/Assets/Scripts/Managers/CombatManager.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
-    
+    public const int defaultBoardSize = 6;
+
+    public PuzzlePile drawPile, discardPile, handPile;
+    public PuzzleBoard puzzleBoard;
+
+    private void Start()
+    {
+        drawPile = new PuzzlePile();
+        discardPile = new PuzzlePile();
+        handPile = new PuzzlePile();
+        puzzleBoard = new PuzzleBoard(defaultBoardSize);
+    }
 }
---
+++ Puzzle Jam/Assets/Scripts/Puzzle/PuzzlePile.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePile
+{
+    public List<PuzzlePiece> puzzlePieces;
+
+    public PuzzlePile()
+    {
+        puzzlePieces = new List<PuzzlePiece>();
+    }
+
+    public void AddPuzzlePiece(PuzzlePiece puzzlePiece)
+    {
+        if (puzzlePiece != null) puzzlePieces.Add(puzzlePiece);
+    }
+
+    public void AddPuzzlePieces(List<PuzzlePiece> newPuzzlePieces)
+    {
+        if (newPuzzlePieces != null) puzzlePieces.AddRange(newPuzzlePieces);
+    }
+
+    public List<PuzzlePiece> DiscardPile()
+    {
+        return puzzlePieces;
+    }
+
+    public PuzzlePiece DrawPuzzlePiece()
+    {
+        if (puzzlePieces.Count == 0) return null;
+        else
+        {
+            PuzzlePiece puzzlePiece = puzzlePieces[0];
+            puzzlePieces.RemoveAt(0);
+            return puzzlePiece;
+        }
+    }
+
+    public void EmptyPile()
+    {
+        puzzlePieces.Clear();
+    }
+
+    public void ShufflePile()
+    {
+        List<PuzzlePiece> newList = new List<PuzzlePiece>();
+        while (puzzlePieces.Count > 0)
+        {
+            newList.Insert(Random.Range(0, newList.Count), DrawPuzzlePiece());
+        }
+        puzzlePieces.AddRange(newList);
+    }
+}
