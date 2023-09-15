28c86a7a4fa39f93e93d9fd969be656d806b3fd8
Caleb Tutor <cmtutor123@gmail.com>
2023-09-15 10:56:01 -0400

    started on health bar manager

---
+++ Puzzle Jam/Assets/Scripts/Managers/HealthBarManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Manages a health bar for the player or an enemy
+/// </summary>
+public class HealthBarManager : MonoBehaviour
+{
+    [Header("Health Bar")]
+    [SerializeField] private Image frontHealthBar;
+    [SerializeField] private Image backHealthBar;
+    [SerializeField] private Image background;
+    [Header("Colors")]
+    [SerializeField] private Color healthColor;
+    [SerializeField] private Color damageColor;
+    [SerializeField] private Color healColor;
+    [Header("Animation Values")]
+    [SerializeField] private float chipSpeed;
+
+    private float lerpTimer = 0;
+
+    private int maxHealth;
+    private int currentHealth;
+
+    void Update()
+    {
+        UpdateHealthBarUI();
+    }
+
+    /// <summary>
+    /// Sets the current health
+    /// </summary>
+    /// <param name="currentHealth">The value for the current health</param>
+    public void SetHealth(int currentHealth)
+    {
+        this.currentHealth = currentHealth;
+        lerpTimer = 0;
+    }
+
+    /// <summary>
+    /// Sets the current health and the max health
+    /// </summary>
+    /// <param name="currentHealth">The value for the current health</param>
+    /// <param name="maxHealth">The value for the max health</param>
+    public void SetHealth(int currentHealth, int maxHealth)
+    {
+        this.currentHealth = currentHealth;
+        this.maxHealth = maxHealth;
+    }
+
+    /// <summary>
+    /// Sets the max health
+    /// </summary>
+    /// <param name="maxHealth">The value for the max health</param>
+    public void SetMaxHealth(int maxHealth)
+    {
+        this.maxHealth = maxHealth;
+    }
+
+    /// <summary>
+    /// Updates the ui of the health bar
+    /// </summary>
+    public void UpdateHealthBarUI()
+    {
+        float fillFront = frontHealthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float hFraction;
+        if (maxHealth <= 0 || currentHealth <= 0) hFraction = 0;
+        else hFraction = currentHealth / maxHealth;
+        if (fillBack > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = damageColor;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, hFraction, percentComplete);
+        }
+        if (fillFront < hFraction)
+        {
+            backHealthBar.color = healColor;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+
+    /// <summary>
+    /// Hides the health bar ui
+    /// </summary>
+    public void HideHealthBar()
+    {
+
+    }
+
+    /// <summary>
+    /// Shows the health bar ui
+    /// </summary>
+    public void ShowHealthBar()
+    {
+
+    }
+}
